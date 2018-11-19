1beb5fa46142aeefa9adee34de89c260041eff71
Carl Thuringer <carl@thuringer.us>
2018-11-18 23:02:43 -0600

    It writes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import fs from "fs";
 import MarkdownIt from "markdown-it";
-const md = new MarkdownIt({ typographer: true });
 
-function promisingOne<A1, T>(
+type Dict = { [index: string]: string };
+
+const promisingOne = <A1, T>(
   fn: (arg: A1, cb: (err: Error, res: T) => void) => void,
   arg: A1
-): Promise<T> {
-  return new Promise((resolve, reject) =>
+): Promise<T> =>
+  new Promise((resolve, reject) =>
     fn(arg, (err: Error, res: T) => {
       if (err) {
         reject(err);
@@ -15,20 +16,77 @@ function promisingOne<A1, T>(
       }
     })
   );
-}
-export async function build() {
-  const contents = await promisingOne<fs.PathLike, string[]>(
-    fs.readdir,
-    "content"
-  );
-  console.log(contents);
-  let renders: { [index: string]: string } = {};
-  for (let file of contents) {
-    const fileBuffer = await promisingOne(fs.readFile, `content/${file}`);
-    const rendered = md.render(fileBuffer.toString());
-    renders[file] = rendered;
-  }
+
+type TemplateFn = (binds: Dict) => string;
+
+export type Environment = {
+  content: Dict;
+  templates: {
+    [index: string]: TemplateFn;
+  };
+};
+
+export const build = async () => {
+  let env = await init();
+  let outputs = compileTemplates(env);
+  await write(outputs);
+
+  // COMPILE
+  // OUTPUT
+
+  // let renders: { [index: string]: string } = {};
+  // for (let file of contents) {
+  //   const fileBuffer = await promisingOne(fs.readFile, `content/${file}`);
+  //   const rendered = md.render(fileBuffer.toString());
+  //   renders[file] = rendered;
+  // }
 
   // for (let render in renders) {
   // }
-}
+};
+
+const init = async (): Promise<Environment> => {
+  const md = new MarkdownIt({ typographer: true });
+  let env: Environment = { content: {}, templates: { index: indexTemplate } };
+  const contentFiles = await promisingOne<fs.PathLike, string[]>(
+    fs.readdir,
+    "content"
+  );
+
+  for (let file of contentFiles) {
+    const fileBuffer = await promisingOne(fs.readFile, `content/${file}`);
+    const rendered = md.render(fileBuffer.toString());
+    env.content[file] = rendered;
+  }
+
+  return env;
+};
+
+const indexTemplate: TemplateFn = binds =>
+  `<DOCTYPE !html>
+  <html>
+  <body>
+  ${binds.body}
+  </body>
+  </html>
+  `;
+
+const mapDict = (xs: Dict, fn: (x: string) => string): Dict => {
+  let r: Dict = {};
+  for (let x in xs) {
+    r[x] = fn(xs[x]);
+  }
+  return r;
+};
+
+const compileTemplates = (env: Environment) =>
+  mapDict(env.content, c => env.templates.index({ body: c }));
+
+const write = async (compiledTemplates: Dict) => {
+  for (let template in compiledTemplates) {
+    await fs.writeFileSync(
+      `dist/${template}.html`,
+      compiledTemplates[template]
+    );
+  }
+};
