de0c4d221f02ded324c843b450382a06f2f68f73
Carl Thuringer <carl@thuringer.us>
2018-11-23 19:14:42 -0500

    Web Server

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # Carlthuringer Com
+
 Website production engine for DAT
 
 ## SETUP
-* NVM: `curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash`
-* NPM: `npm install`
-* DAT: `npm install -g dat`
+
+- NVM: `curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash`
+- NPM: `npm install`
+- DAT: `npm install -g dat`
 
 ## Development
-* Build: `npm run build`
-* Publish: `dat share dist`
+
+- Build: `npm run build`
+- Publish: `dat share dist`
   Wait a few seconds for hashbase to pick up the new files.
+- Make a self-signed certificate:
+  ```
+  mkdir tmp
+  openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' \
+    -keyout tmp/localhost-privkey.pem -out tmp/localhost-cert.pem
+  ```
 
 ## Key management
-* Get Key: `dat keys export dist`
-* Set Key: `dat keys import`
+
+- Get Key: `dat keys export dist`
+- Set Key: `dat keys import`
---
+++ src/server/index.ts
@@ -0,0 +1,78 @@
+import http from "http";
+import { readFile } from "fs";
+import { extname, join } from "path";
+
+type MIME_TYPE_MAP = { [index: string]: string };
+const MIME_TYPE_MAP: MIME_TYPE_MAP = {
+  ".html": "text/html",
+  ".png": "image/png"
+};
+
+class Server {
+  serverRoot: string;
+
+  constructor(root: string) {
+    this.serverRoot = root;
+  }
+
+  respondToError = (err: NodeJS.ErrnoException, res: http.ServerResponse) => {
+    this.log("ERR", err.toString());
+    if (err.code == "ENOENT") {
+      res.writeHead(404);
+    } else {
+      res.writeHead(500);
+    }
+  };
+
+  log = (progName: string, message: string) => {
+    console.log(`[${process.hrtime()[0]}][${progName}] - ${message}`);
+  };
+
+  start = () => {
+    const server = http.createServer();
+
+    server.on(
+      "request",
+      (req: http.IncomingMessage, res: http.ServerResponse) => {
+        const reqPath = req.url;
+        const reqMethod = req.method;
+
+        let fullPath: string;
+        if (
+          typeof reqPath == "string" &&
+          reqPath.length > 0 &&
+          reqPath != "/"
+        ) {
+          if (extname(reqPath) == "") {
+            fullPath = join(this.serverRoot, reqPath + ".html");
+          } else {
+            fullPath = join(this.serverRoot, reqPath);
+          }
+        } else {
+          fullPath = join(this.serverRoot, "index.html");
+        }
+
+        let mimeType = MIME_TYPE_MAP[extname(fullPath)];
+        if (!mimeType) {
+          mimeType = "application/octet-stream";
+          this.log("PRS", `Unknown Mime Type for ${fullPath}`);
+        }
+
+        console.log(reqMethod, fullPath);
+        readFile(fullPath, (err, data) => {
+          if (err) {
+            this.respondToError(err, res);
+          } else {
+            res.setHeader("Content-Type", mimeType);
+            res.write(data);
+          }
+          res.end();
+        });
+      }
+    );
+
+    server.listen(8080);
+  };
+}
+
+export default (serverRoot: string) => new Server(serverRoot).start();
